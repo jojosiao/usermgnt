c558d979437bad700b769a95ebdcd1231eef1b36
Joubert Siao <joubertsiao@gmail.com>
2019-02-15 23:36:33 -0500

    20190216 changes.

--- forms/password_reset.py
+++ forms/password_reset.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class PasswordResetForm(forms.Form):
+class forgotpasswordform(forms.Form):
     current_password = forms.CharField()
     new_password = forms.CharField()
     retype_password = forms.CharField()
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-
+from django.conf import settings as SETTINGS
 # to obtain greater control in user authentication
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
@@ -9,7 +9,7 @@ from django.contrib.auth import views as auth_views
 
 from .forms.user_login import userloginform
 #from .forms.password_change import passwordchangeform
-#from .forms.password_reset import passwordresetform
+from .forms.password_reset import forgotpasswordform
 
 
 urlpatterns = [
@@ -27,9 +27,33 @@ urlpatterns = [
 
     path('password_change/', auth_views.PasswordChangeView.as_view(
             template_name = "usermgnt/password_change_form.html",
-            success_url = "/users/password_change/done/"
+            success_url = SETTINGS.DEFAULT_HOMEPAGE,
         ),
         name = "usermgnt-password_change"
+    ),
+
+    path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(
+            template_name = "usermgnt/password_change_done.html"
+        )
+    ),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+            template_name = "usermgnt/password_reset_form.html",
+            form_class = forgotpasswordform,
+            success_url = SETTINGS.DEFAULT_HOMEPAGE,
+            email_template_name = "usermgnt/emails/password_reset_email.html",
+            subject_template_name = "usermgnt/password_reset_subject.txt",
+
+        ),
+        name = "usermgnt-password_reset"
+    ),
+
+    path('password_reset_confirm/<uuid:uidb64>/<str:token>/',auth_views.PasswordResetConfirmView.as_view(
+            template_name = "usermgnt/password_reset_confirm_form.html",
+            
+
+        ),
+        name = "usermgnt-password_reset_confirm"
     ),
 
     path('logout/',auth_views.LogoutView.as_view(template_name="usermgnt/logout.html"), name="logout"),
